855a6b842cf6d9173457e8bb13d9763418a6d8e7
HanDongHun <95519378+mrsuit0114@users.noreply.github.com>
2023-06-10 10:32:28 +0900

    unity_junior4 Fin
    assingment Fin

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5.0f;
+    private Rigidbody playerRb;
+    private float powerStrength = 15.0f;
+
+    public bool hasPowerup;
+    public GameObject powerupIndicator;
+    private GameObject focalPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
+        // 자식 오브젝트를 파악할때 뭘쓰나? 퍼블릭이 아닐때는 메서드로 찾아야할것인데
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
+        float forwardInput = Input.GetAxis("Vertical");
+        playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+    }
+
+    private void OnTriggerEnter(Collider other)  //트리거 파악에 용이
+    {
+        if (other.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            Destroy(other.gameObject);
+            StartCoroutine(PowerupCountdownRoutine());
+            powerupIndicator.gameObject.SetActive(true);
+        }
+        /*if(other.CompareTag("Enemy") && hasPowerup)
+        {
+            Rigidbody enemyRigidbody = other.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = other.gameObject.transform.position - transform.position;
+            enemyRigidbody.AddForce(awayFromPlayer * powerStrength, ForceMode.Impulse);
+        }*/ // 작동안함 트리거이면 물리를 다루는게 말이안됨 통과하기 때문에
+    }
+
+    IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerup = false;
+        powerupIndicator.gameObject.SetActive(false);
+    }
+    private void OnCollisionEnter(Collision collision)  //이후 물리를 사용할 때 용이
+    {
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = collision.gameObject.transform.position - transform.position;
+            enemyRigidbody.AddForce(awayFromPlayer * powerStrength, ForceMode.Impulse);
+        }
+    }
+}
